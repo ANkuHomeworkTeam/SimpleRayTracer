aba03d292f7ed09987c253aa33d9b732df7e3095
hrl <hrl@hrl.168.18.4>
2020-05-28 15:20:43 +0800

    ,...

--- src/include/renderInfo.hpp
+++ src/include/renderInfo.hpp
@@ -3,20 +3,64 @@
 #ifndef __RENDER_INFO_HPP__
 #define __RENDER_INFO_HPP__
 
-struct MaterialInfo
+#include "renderer.hpp"
+namespace Renderer
 {
-    
-};
+    enum class MaterialType
+    {
+        LAMBERTAIN,
+        PHONG,
+        SPECULAR,
+        GLASS  
+    };
 
-struct ObjectInfo
-{
-    
-};
+    enum class ObjectType
+    {
+        Sphere,
+        Triangle
+    };
 
-struct TextureInfo
-{
-    
-};
+    enum class TextureType
+    {
+        Solid
+    };
 
+    struct MaterialInfo
+    {
+        MaterialType    type;
+        id_t            id;
+        id_t            texture;
+        Vec3            v1;
+        Vec3            v2;
+        Vec3            v3;
+        float           f1;
+        float           f2;
+        float           f3;
+    };
+
+    struct ObjectInfo
+    {
+        ObjectType      type;
+        id_t            id;
+        id_t            material;
+        Vec3            v1;
+        Vec3            v2;
+        Vec3            v3;
+        float           f1;
+        float           f2;
+        float           f3;
+        int             i1;
+        int             i2;
+        int             i3;
+    };
+
+    struct TextureInfo
+    {
+        TextureType     type;
+        id_t            id;
+        Vec3            v1;
+    };
+
+}; // namespace Renderer
 
 #endif
--- src/src/renderer.cpp
+++ src/src/renderer.cpp
@@ -7,6 +7,10 @@ using namespace std;
 
 RenderEnv globalEnv = RenderEnv::UNDEFINE;
 
+std::vector<Vec3> vertexBuffer;
+std::vector<TextureInfo> textureBuffer;
+std::vector<MaterialInfo> materialBuffer;
+std::vector<ObjectInfo> objectBuffer;
 
 bool checkCudaSupport();
 
@@ -32,5 +36,56 @@ ErrorCode init(RenderEnv env) {
     default:
         return ErrorCode::ENV_NOT_SUPPORT;
     }
+    Texture::createSolid({1, 1, 1});
+    Material::createLambertain(0);
     return ErrorCode::SUCCESS;
+}
+
+id_t addVertex(const Vec3& p) {
+    vertexBuffer.push_back(p);
+    return vertexBuffer.size() - 1;
+}
+
+id_t Texture::createSolid(const Vec3& rgb) {
+    TextureInfo temp;
+    int index = textureBuffer.size();
+    temp.type = TextureType::Solid;
+    temp.id   = index;
+    temp.v1   = rgb;
+    textureBuffer.push_back(temp);
+    return index;
+}
+
+id_t Material::createLambertain(id_t texture) {
+    MaterialInfo temp;
+    int index    = materialBuffer.size();
+    temp.type    = MaterialType::LAMBERTAIN;
+    temp.id      = index;
+    temp.texture = texture;
+    materialBuffer.push_back(temp);
+    return index;
+}
+
+id_t Object::createSphere(const Vec3& position, float radius, id_t material) {
+    ObjectInfo temp;
+    int index     = objectBuffer.size();
+    temp.type     = ObjectType::Sphere;
+    temp.id       = index;
+    temp.material = material;
+    temp.v1       = position;
+    temp.f1       = radius;
+    objectBuffer.push_back(temp);
+    return index;
+}
+
+id_t Object::createTriangle(const Vec3& p1, const Vec3& p2, const Vec3& p3, id_t material) {
+    ObjectInfo temp;
+    int index     = objectBuffer.size();
+    temp.type     = ObjectType::Triangle;
+    temp.id       = index;
+    temp.material = material;
+    temp.i1       = addVertex(p1);
+    addVertex(p2);
+    addVertex(p3);
+    return index;
 }
